4205333dfacfb02030a1b207f26d8c1de0da5251
Dmitrii Vlasov <gcp@moslex.ru>
2019-11-07 16:43:23 +0300

    systemd

---
+++ systemd/README.md
@@ -0,0 +1,2 @@
+`bigbrother`  systemd service
+`bigbrother.sh` script check config apache every 60s  
---
+++ systemd/bigbrother.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while (true);do
+CONF_R="/etc/apache2/sites-available/000-default.conf"
+CONF_O="/root/conf_apache/000-default.conf"
+MD_O="$(md5sum $CONF_O| awk '{print $1}')"
+MD_R="$(md5sum $CONF_R| awk '{print $1}')"
+
+if [ "$MD_O" != "$MD_R" ] ; then 
+	echo "$(date) Config is change. Restore apache conf." >> /var/log/bigbrother.log
+        cp $CONF_O $CONF_R
+	     
+ else 
+	 echo "$(date) Config is OK not change, return 60s." >> /var/log/bigbrother.log
+fi
+ 
+sleep 60
+done;
